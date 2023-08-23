9eba3428a23a768a79a57cf1d7bc175e9b91b5f7
Manuka_Dahanayake <manukaud98@gmail.com>
2023-08-23 17:52:59 +0530

    fixed dropdowns

--- src/Components/Quotes/Duration.tsx
+++ src/Components/Quotes/Duration.tsx
@@ -8,7 +8,16 @@ const DurationInput = () => {
           id="tourDuration"
           className="w-full md:w-[150px] bg-transparent h-10 rounded-md border border-gray-300 bg-opacity-0 text-sm text-black md:text-white pl-3"
         >
+          <option value="1">1 Day</option>
+          <option value="2">2 Days</option>
+          <option value="3">3 Days</option>
+          <option value="4">4 Days</option>
+          <option value="5">5 Days</option>
           <option value="6">6 Days</option>
+          <option value="7">7 Days</option>
+          <option value="8">8 Days</option>
+          <option value="9">9 Days</option>
+          <option value="10">10 Days</option>
         </select>
       </div>
     );
